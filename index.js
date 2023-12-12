4dd82db3abeeb8514ac41fc86625c118eb22b4bf
MarcosIyama <marcosiyama@gmail.com>
2023-12-12 18:19:14 -0300

    1ª atualização

---
+++ index.js
@@ -0,0 +1,24 @@
+function ranking(heroWins, heroLosses){
+    return heroWins - heroLosses;
+}
+
+let heroRanking = ranking(40, 50)
+
+if(heroRanking <= 10){
+    heroLevel = "Ferro"
+}else if(heroRanking <= 20){
+    heroLevel = "Bronze"
+}else if(heroRanking <= 50){
+    heroLevel = "Prata"
+}else if(heroRanking <= 80){
+    heroLevel = "Ouro"
+}else if(heroRanking <= 90){
+    heroLevel = "Diamante"
+}else if(heroRanking <= 100){
+    heroLevel = "Lendário"
+}else if(heroRanking >= 101){
+    heroLevel = "Imortal"
+}
+
+
+console.log("O Herói tem de saldo de " + heroRanking + " vitórias e está no nível: " + heroLevel + ".")
